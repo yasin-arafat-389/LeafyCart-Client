5d5768531c6582aa6fd5cf5d8a4a4d1f89a2750f
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-07-09 11:57:10 +0600

    Designed categories grid

--- src/components/CategoriesGrid.tsx
+++ src/components/CategoriesGrid.tsx
@@ -1,98 +1,98 @@
-// import indoorPlants from "../assets/indoor-plants.jpg";
-// import outdoorPlants from "../assets/outdoor-tree.jpeg";
-// import fruitPlants from "../assets/fruit-tree.jpg";
-// import flowerPlants from "../assets/flowering-tree.webp";
+import indoorPlants from "../assets/indoor-plants.jpg";
+import outdoorPlants from "../assets/outdoor-tree.jpeg";
+import fruitPlants from "../assets/fruit-tree.jpg";
+import flowerPlants from "../assets/flowering-tree.webp";
 
-// const CategoriesGrid = () => {
-//   return (
-//     <div>
-//       <div className="bg-[#EEEDEB] dark:bg-gray-800 h-full py-6 sm:py-8 lg:py-12">
-//         <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
-//           <div className="mb-4 flex items-center justify-center gap-8 sm:mb-8 md:mb-12">
-//             <div className="flex items-center justify-center gap-12">
-//               <h2 className="text-2xl font-bold text-gray-800 lg:text-4xl dark:text-white">
-//                 Browse by <span className="text-[#1A5319]">Category</span>
-//               </h2>
-//             </div>
-//           </div>
+const CategoriesGrid = () => {
+  return (
+    <div>
+      <div className="bg-[#EEEDEB] dark:bg-gray-800 h-full py-6 sm:py-8 lg:py-12">
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <div className="mb-4 flex items-center justify-center gap-8 sm:mb-8 md:mb-12">
+            <div className="flex items-center justify-center gap-12">
+              <h2 className="text-2xl font-bold text-gray-800 lg:text-4xl dark:text-white">
+                Browse by <span className="text-[#1A5319]">Category</span>
+              </h2>
+            </div>
+          </div>
 
-//           <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
-//             <a
-//               href="#"
-//               className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
-//             >
-//               <img
-//                 src={indoorPlants}
-//                 loading="lazy"
-//                 alt="Photo by Minh Pham"
-//                 className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-//               />
+          <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">
+            <a
+              href="#"
+              className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
+            >
+              <img
+                src={indoorPlants}
+                loading="lazy"
+                alt="Photo by Minh Pham"
+                className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+              />
 
-//               <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+              <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-//               <span className="relative bg-[#508D4E] p-2 rounded-lg ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
-//                 Indoor Plants
-//               </span>
-//             </a>
+              <span className="relative bg-[#508D4E] p-2 rounded-lg ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
+                Indoor Plants
+              </span>
+            </a>
 
-//             <a
-//               href="#"
-//               className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
-//             >
-//               <img
-//                 src={outdoorPlants}
-//                 loading="lazy"
-//                 alt="Photo by Magicle"
-//                 className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-//               />
+            <a
+              href="#"
+              className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
+            >
+              <img
+                src={outdoorPlants}
+                loading="lazy"
+                alt="Photo by Magicle"
+                className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+              />
 
-//               <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+              <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-//               <span className="relative ml-4 bg-[#508D4E] p-2 rounded-lg mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
-//                 Outdoor Trees
-//               </span>
-//             </a>
+              <span className="relative ml-4 bg-[#508D4E] p-2 rounded-lg mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
+                Outdoor Trees
+              </span>
+            </a>
 
-//             <a
-//               href="#"
-//               className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
-//             >
-//               <img
-//                 src={fruitPlants}
-//                 loading="lazy"
-//                 alt="Photo by Martin Sanchez"
-//                 className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-//               />
+            <a
+              href="#"
+              className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80"
+            >
+              <img
+                src={fruitPlants}
+                loading="lazy"
+                alt="Photo by Martin Sanchez"
+                className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+              />
 
-//               <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+              <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-//               <span className="relative ml-4 bg-[#508D4E] p-2 rounded-lg mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
-//                 Fruit-Bearing Trees
-//               </span>
-//             </a>
+              <span className="relative ml-4 bg-[#508D4E] p-2 rounded-lg mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
+                Fruit-Bearing Trees
+              </span>
+            </a>
 
-//             <a
-//               href="#"
-//               className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
-//             >
-//               <img
-//                 src={flowerPlants}
-//                 loading="lazy"
-//                 alt="Photo by Lorenzo Herrera"
-//                 className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
-//               />
+            <a
+              href="#"
+              className="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80"
+            >
+              <img
+                src={flowerPlants}
+                loading="lazy"
+                alt="Photo by Lorenzo Herrera"
+                className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
+              />
 
-//               <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
+              <div className="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50"></div>
 
-//               <span className="relative ml-4 bg-[#508D4E] p-2 rounded-lg mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
-//                 Flowering Trees
-//               </span>
-//             </a>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+              <span className="relative ml-4 bg-[#508D4E] p-2 rounded-lg mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">
+                Flowering Trees
+              </span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default CategoriesGrid;
+export default CategoriesGrid;
