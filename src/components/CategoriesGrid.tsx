897ab62da806b66d75bc392e28d413c85302fa5e
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-07-09 15:34:19 +0600

    Designed categories grid

--- src/components/CategoriesGrid.tsx
+++ src/components/CategoriesGrid.tsx
@@ -11,7 +11,7 @@ const CategoriesGrid = () => {
           <div className="mb-4 flex items-center justify-center gap-8 sm:mb-8 md:mb-12">
             <div className="flex items-center justify-center gap-12">
               <h2 className="text-2xl font-bold text-gray-800 lg:text-4xl dark:text-white">
-                Browse by <span className="text-[#1A5319]">Category</span>
+                Browse by <span className="text-[#508D4E]">Category</span>
               </h2>
             </div>
           </div>
